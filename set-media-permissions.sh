421ba1478e15b0d83daaa5bfc1643071c450a787
Kevinesan Pillay <kevin.pillay@is.co.za>
2018-10-13 21:23:43 +0200

    first commit

---
+++ set-media-permissions.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#title           :set-media-permissions.sh
+#description     :Set proper permissions to allow deletion of media from plex, and proper indexing access
+#author		 :Kevin Pillay
+#date            :20181013
+#version         :0.1    
+#usage		 :bash set-media-permissions.sh
+#notes           :
+#bash_version    :4.1.5(1)-release
+#==============================================================================
+
+
